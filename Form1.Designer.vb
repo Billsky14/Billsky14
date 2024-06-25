8ee342404ff9b5496cf5cc0e9a521528649cd39d
Billsky14 <152649957+Billsky14@users.noreply.github.com>
2024-06-25 21:00:39 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,417 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cb1 = New System.Windows.Forms.CheckBox()
+        Me.cb2 = New System.Windows.Forms.CheckBox()
+        Me.cb3 = New System.Windows.Forms.CheckBox()
+        Me.cb4 = New System.Windows.Forms.CheckBox()
+        Me.cb5 = New System.Windows.Forms.CheckBox()
+        Me.cb6 = New System.Windows.Forms.CheckBox()
+        Me.txharga1 = New System.Windows.Forms.TextBox()
+        Me.txharga2 = New System.Windows.Forms.TextBox()
+        Me.txharga3 = New System.Windows.Forms.TextBox()
+        Me.txharga4 = New System.Windows.Forms.TextBox()
+        Me.txharga5 = New System.Windows.Forms.TextBox()
+        Me.txharga6 = New System.Windows.Forms.TextBox()
+        Me.byr1 = New System.Windows.Forms.TextBox()
+        Me.byr2 = New System.Windows.Forms.TextBox()
+        Me.byr3 = New System.Windows.Forms.TextBox()
+        Me.byr4 = New System.Windows.Forms.TextBox()
+        Me.byr5 = New System.Windows.Forms.TextBox()
+        Me.byr6 = New System.Windows.Forms.TextBox()
+        Me.shrg1 = New System.Windows.Forms.TextBox()
+        Me.shrg2 = New System.Windows.Forms.TextBox()
+        Me.shrg3 = New System.Windows.Forms.TextBox()
+        Me.shrg4 = New System.Windows.Forms.TextBox()
+        Me.shrg5 = New System.Windows.Forms.TextBox()
+        Me.shrg6 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtotpem = New System.Windows.Forms.TextBox()
+        Me.txdiskon = New System.Windows.Forms.TextBox()
+        Me.txtagihan = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial Narrow", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(28, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(181, 33)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "MENU PILIHAN"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial Narrow", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(245, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 33)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "HARGA"
+        '
+        'cb1
+        '
+        Me.cb1.AutoSize = True
+        Me.cb1.Font = New System.Drawing.Font("Arial Narrow", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.cb1.Location = New System.Drawing.Point(28, 112)
+        Me.cb1.Name = "cb1"
+        Me.cb1.Size = New System.Drawing.Size(131, 31)
+        Me.cb1.TabIndex = 2
+        Me.cb1.Text = "Mie Pangsit"
+        Me.cb1.UseVisualStyleBackColor = True
+        '
+        'cb2
+        '
+        Me.cb2.AutoSize = True
+        Me.cb2.Font = New System.Drawing.Font("Arial Narrow", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.cb2.Location = New System.Drawing.Point(28, 161)
+        Me.cb2.Name = "cb2"
+        Me.cb2.Size = New System.Drawing.Size(123, 31)
+        Me.cb2.TabIndex = 3
+        Me.cb2.Text = "Mie Ayam "
+        Me.cb2.UseVisualStyleBackColor = True
+        '
+        'cb3
+        '
+        Me.cb3.AutoSize = True
+        Me.cb3.Font = New System.Drawing.Font("Arial Narrow", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.cb3.Location = New System.Drawing.Point(28, 206)
+        Me.cb3.Name = "cb3"
+        Me.cb3.Size = New System.Drawing.Size(112, 31)
+        Me.cb3.TabIndex = 4
+        Me.cb3.Text = "Mie Aceh"
+        Me.cb3.UseVisualStyleBackColor = True
+        '
+        'cb4
+        '
+        Me.cb4.AutoSize = True
+        Me.cb4.Font = New System.Drawing.Font("Arial Narrow", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.cb4.Location = New System.Drawing.Point(28, 252)
+        Me.cb4.Name = "cb4"
+        Me.cb4.Size = New System.Drawing.Size(93, 31)
+        Me.cb4.TabIndex = 5
+        Me.cb4.Text = "Nasgor"
+        Me.cb4.UseVisualStyleBackColor = True
+        '
+        'cb5
+        '
+        Me.cb5.AutoSize = True
+        Me.cb5.Font = New System.Drawing.Font("Arial Narrow", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.cb5.Location = New System.Drawing.Point(28, 299)
+        Me.cb5.Name = "cb5"
+        Me.cb5.Size = New System.Drawing.Size(113, 31)
+        Me.cb5.TabIndex = 6
+        Me.cb5.Text = "Teh Botol"
+        Me.cb5.UseVisualStyleBackColor = True
+        '
+        'cb6
+        '
+        Me.cb6.AutoSize = True
+        Me.cb6.Font = New System.Drawing.Font("Arial Narrow", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.cb6.Location = New System.Drawing.Point(28, 345)
+        Me.cb6.Name = "cb6"
+        Me.cb6.Size = New System.Drawing.Size(148, 31)
+        Me.cb6.TabIndex = 7
+        Me.cb6.Text = "Es Teh Manis"
+        Me.cb6.UseVisualStyleBackColor = True
+        '
+        'txharga1
+        '
+        Me.txharga1.BackColor = System.Drawing.Color.Gray
+        Me.txharga1.Location = New System.Drawing.Point(245, 115)
+        Me.txharga1.Name = "txharga1"
+        Me.txharga1.Size = New System.Drawing.Size(156, 27)
+        Me.txharga1.TabIndex = 8
+        '
+        'txharga2
+        '
+        Me.txharga2.BackColor = System.Drawing.Color.Gray
+        Me.txharga2.Location = New System.Drawing.Point(245, 165)
+        Me.txharga2.Name = "txharga2"
+        Me.txharga2.Size = New System.Drawing.Size(156, 27)
+        Me.txharga2.TabIndex = 9
+        '
+        'txharga3
+        '
+        Me.txharga3.BackColor = System.Drawing.Color.Gray
+        Me.txharga3.Location = New System.Drawing.Point(245, 210)
+        Me.txharga3.Name = "txharga3"
+        Me.txharga3.Size = New System.Drawing.Size(156, 27)
+        Me.txharga3.TabIndex = 10
+        '
+        'txharga4
+        '
+        Me.txharga4.BackColor = System.Drawing.Color.Gray
+        Me.txharga4.Location = New System.Drawing.Point(245, 256)
+        Me.txharga4.Name = "txharga4"
+        Me.txharga4.Size = New System.Drawing.Size(156, 27)
+        Me.txharga4.TabIndex = 11
+        '
+        'txharga5
+        '
+        Me.txharga5.BackColor = System.Drawing.Color.Gray
+        Me.txharga5.Location = New System.Drawing.Point(245, 303)
+        Me.txharga5.Name = "txharga5"
+        Me.txharga5.Size = New System.Drawing.Size(156, 27)
+        Me.txharga5.TabIndex = 12
+        '
+        'txharga6
+        '
+        Me.txharga6.BackColor = System.Drawing.Color.Gray
+        Me.txharga6.Location = New System.Drawing.Point(245, 349)
+        Me.txharga6.Name = "txharga6"
+        Me.txharga6.Size = New System.Drawing.Size(156, 27)
+        Me.txharga6.TabIndex = 13
+        '
+        'byr1
+        '
+        Me.byr1.Location = New System.Drawing.Point(426, 116)
+        Me.byr1.Name = "byr1"
+        Me.byr1.Size = New System.Drawing.Size(65, 27)
+        Me.byr1.TabIndex = 14
+        '
+        'byr2
+        '
+        Me.byr2.Location = New System.Drawing.Point(426, 165)
+        Me.byr2.Name = "byr2"
+        Me.byr2.Size = New System.Drawing.Size(65, 27)
+        Me.byr2.TabIndex = 15
+        '
+        'byr3
+        '
+        Me.byr3.Location = New System.Drawing.Point(426, 210)
+        Me.byr3.Name = "byr3"
+        Me.byr3.Size = New System.Drawing.Size(65, 27)
+        Me.byr3.TabIndex = 16
+        '
+        'byr4
+        '
+        Me.byr4.Location = New System.Drawing.Point(426, 256)
+        Me.byr4.Name = "byr4"
+        Me.byr4.Size = New System.Drawing.Size(65, 27)
+        Me.byr4.TabIndex = 17
+        '
+        'byr5
+        '
+        Me.byr5.Location = New System.Drawing.Point(426, 302)
+        Me.byr5.Name = "byr5"
+        Me.byr5.Size = New System.Drawing.Size(65, 27)
+        Me.byr5.TabIndex = 18
+        '
+        'byr6
+        '
+        Me.byr6.Location = New System.Drawing.Point(426, 349)
+        Me.byr6.Name = "byr6"
+        Me.byr6.Size = New System.Drawing.Size(65, 27)
+        Me.byr6.TabIndex = 19
+        '
+        'shrg1
+        '
+        Me.shrg1.Location = New System.Drawing.Point(510, 116)
+        Me.shrg1.Name = "shrg1"
+        Me.shrg1.Size = New System.Drawing.Size(149, 27)
+        Me.shrg1.TabIndex = 20
+        '
+        'shrg2
+        '
+        Me.shrg2.Location = New System.Drawing.Point(510, 165)
+        Me.shrg2.Name = "shrg2"
+        Me.shrg2.Size = New System.Drawing.Size(149, 27)
+        Me.shrg2.TabIndex = 21
+        '
+        'shrg3
+        '
+        Me.shrg3.Location = New System.Drawing.Point(510, 210)
+        Me.shrg3.Name = "shrg3"
+        Me.shrg3.Size = New System.Drawing.Size(149, 27)
+        Me.shrg3.TabIndex = 22
+        '
+        'shrg4
+        '
+        Me.shrg4.Location = New System.Drawing.Point(510, 256)
+        Me.shrg4.Name = "shrg4"
+        Me.shrg4.Size = New System.Drawing.Size(149, 27)
+        Me.shrg4.TabIndex = 23
+        '
+        'shrg5
+        '
+        Me.shrg5.Location = New System.Drawing.Point(510, 303)
+        Me.shrg5.Name = "shrg5"
+        Me.shrg5.Size = New System.Drawing.Size(149, 27)
+        Me.shrg5.TabIndex = 24
+        '
+        'shrg6
+        '
+        Me.shrg6.Location = New System.Drawing.Point(510, 348)
+        Me.shrg6.Name = "shrg6"
+        Me.shrg6.Size = New System.Drawing.Size(149, 27)
+        Me.shrg6.TabIndex = 25
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(35, 412)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(124, 43)
+        Me.Button1.TabIndex = 26
+        Me.Button1.Text = "Clear"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(349, 430)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(166, 25)
+        Me.Label3.TabIndex = 27
+        Me.Label3.Text = "Total Pembelian Rp:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(439, 478)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(76, 25)
+        Me.Label4.TabIndex = 28
+        Me.Label4.Text = "Diskon :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(370, 536)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(145, 25)
+        Me.Label5.TabIndex = 29
+        Me.Label5.Text = "Total Tagihan Rp:"
+        '
+        'txtotpem
+        '
+        Me.txtotpem.Location = New System.Drawing.Point(510, 428)
+        Me.txtotpem.Name = "txtotpem"
+        Me.txtotpem.Size = New System.Drawing.Size(149, 27)
+        Me.txtotpem.TabIndex = 30
+        '
+        'txdiskon
+        '
+        Me.txdiskon.Location = New System.Drawing.Point(510, 478)
+        Me.txdiskon.Name = "txdiskon"
+        Me.txdiskon.Size = New System.Drawing.Size(149, 27)
+        Me.txdiskon.TabIndex = 31
+        '
+        'txtagihan
+        '
+        Me.txtagihan.Location = New System.Drawing.Point(510, 534)
+        Me.txtagihan.Name = "txtagihan"
+        Me.txtagihan.Size = New System.Drawing.Size(149, 27)
+        Me.txtagihan.TabIndex = 32
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(686, 629)
+        Me.Controls.Add(Me.txtagihan)
+        Me.Controls.Add(Me.txdiskon)
+        Me.Controls.Add(Me.txtotpem)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.shrg6)
+        Me.Controls.Add(Me.shrg5)
+        Me.Controls.Add(Me.shrg4)
+        Me.Controls.Add(Me.shrg3)
+        Me.Controls.Add(Me.shrg2)
+        Me.Controls.Add(Me.shrg1)
+        Me.Controls.Add(Me.byr6)
+        Me.Controls.Add(Me.byr5)
+        Me.Controls.Add(Me.byr4)
+        Me.Controls.Add(Me.byr3)
+        Me.Controls.Add(Me.byr2)
+        Me.Controls.Add(Me.byr1)
+        Me.Controls.Add(Me.txharga6)
+        Me.Controls.Add(Me.txharga5)
+        Me.Controls.Add(Me.txharga4)
+        Me.Controls.Add(Me.txharga3)
+        Me.Controls.Add(Me.txharga2)
+        Me.Controls.Add(Me.txharga1)
+        Me.Controls.Add(Me.cb6)
+        Me.Controls.Add(Me.cb5)
+        Me.Controls.Add(Me.cb4)
+        Me.Controls.Add(Me.cb3)
+        Me.Controls.Add(Me.cb2)
+        Me.Controls.Add(Me.cb1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cb1 As CheckBox
+    Friend WithEvents cb2 As CheckBox
+    Friend WithEvents cb3 As CheckBox
+    Friend WithEvents cb4 As CheckBox
+    Friend WithEvents cb5 As CheckBox
+    Friend WithEvents cb6 As CheckBox
+    Friend WithEvents txharga1 As TextBox
+    Friend WithEvents txharga2 As TextBox
+    Friend WithEvents txharga3 As TextBox
+    Friend WithEvents txharga4 As TextBox
+    Friend WithEvents txharga5 As TextBox
+    Friend WithEvents txharga6 As TextBox
+    Friend WithEvents byr1 As TextBox
+    Friend WithEvents byr2 As TextBox
+    Friend WithEvents byr3 As TextBox
+    Friend WithEvents byr4 As TextBox
+    Friend WithEvents byr5 As TextBox
+    Friend WithEvents byr6 As TextBox
+    Friend WithEvents shrg1 As TextBox
+    Friend WithEvents shrg2 As TextBox
+    Friend WithEvents shrg3 As TextBox
+    Friend WithEvents shrg4 As TextBox
+    Friend WithEvents shrg5 As TextBox
+    Friend WithEvents shrg6 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtotpem As TextBox
+    Friend WithEvents txdiskon As TextBox
+    Friend WithEvents txtagihan As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,146 @@
+﻿Public Class Form1
+    Dim harga, subtot As Single
+    Dim nsub, tagihan, n1, n2, n3, n4, n5, n6 As Single
+
+
+    Private Sub cb1_CheckedChanged(sender As Object, e As EventArgs) Handles cb1.CheckedChanged
+        If cb1.Checked = True Then
+            txharga1.BackColor = Color.White
+            txharga1.Text = Format(12000, "#,##")
+            harga = 12000
+            byr1.Focus()
+        Else
+            txharga1.BackColor = Color.Gray
+            txharga1.Text = ""
+            byr1.Text = ""
+        End If
+    End Sub
+    Private Sub cb2_CheckedChanged(sender As Object, e As EventArgs) Handles cb2.CheckedChanged
+        If cb2.Checked = True Then
+            txharga2.BackColor = Color.White
+            txharga2.Text = Format(10000, "#,##")
+            harga = 10000
+            byr2.Focus()
+        Else
+            txharga2.BackColor = Color.Gray
+            txharga2.Text = ""
+            byr2.Text = ""
+        End If
+    End Sub
+    Private Sub cb3_CheckedChanged(sender As Object, e As EventArgs) Handles cb3.CheckedChanged
+        If cb1.Checked = True Then
+            txharga3.BackColor = Color.White
+            txharga3.Text = Format(15000, "#,##")
+            harga = 15000
+            byr3.Focus()
+        Else
+            txharga3.BackColor = Color.Gray
+            txharga3.Text = ""
+            byr3.Text = ""
+        End If
+    End Sub
+    Private Sub cb4_CheckedChanged(sender As Object, e As EventArgs) Handles cb4.CheckedChanged
+        If cb4.Checked = True Then
+            txharga4.BackColor = Color.White
+            txharga4.Text = Format(14000, "#,##")
+            harga = 14000
+            byr4.Focus()
+        Else
+            txharga4.BackColor = Color.Gray
+            txharga4.Text = ""
+            byr4.Text = ""
+        End If
+    End Sub
+    Private Sub cb5_CheckedChanged(sender As Object, e As EventArgs) Handles cb5.CheckedChanged
+        If cb5.Checked = True Then
+            txharga5.BackColor = Color.White
+            txharga5.Text = Format(8000, "#,##")
+            harga = 8000
+            byr5.Focus()
+        Else
+            txharga5.BackColor = Color.Gray
+            txharga5.Text = ""
+            byr5.Text = ""
+        End If
+    End Sub
+
+
+    Private Sub cb6_CheckedChanged(sender As Object, e As EventArgs) Handles cb6.CheckedChanged
+        If cb6.Checked = True Then
+            txharga6.BackColor = Color.White
+            txharga6.Text = Format(3000, "#,##")
+            harga = 3000
+            byr6.Focus()
+        Else
+            txharga6.BackColor = Color.Gray
+            txharga6.Text = ""
+            byr6.Text = ""
+        End If
+    End Sub
+    Private Sub byr1_TextChanged(sender As Object, e As EventArgs) Handles byr1.TextChanged
+        subtot = Val(harga) * Val(byr1.Text)
+        shrg1.Text = Format(subtot, "#,##")
+        n1 = subtot
+        Call hitung()
+    End Sub
+    Private Sub byr2_TextChanged(sender As Object, e As EventArgs) Handles byr2.TextChanged
+        subtot = Val(harga) * Val(byr2.Text)
+        shrg2.Text = Format(subtot, "#,##")
+        n2 = subtot
+        Call hitung()
+    End Sub
+    Private Sub byr3_TextChanged(sender As Object, e As EventArgs) Handles byr3.TextChanged
+        subtot = Val(harga) * Val(byr3.Text)
+        shrg3.Text = Format(subtot, "#,##")
+        n3 = subtot
+        Call hitung()
+    End Sub
+    Private Sub byr4_TextChanged(sender As Object, e As EventArgs) Handles byr4.TextChanged
+        subtot = Val(harga) * Val(byr4.Text)
+        shrg4.Text = Format(subtot, "#,##")
+        n4 = subtot
+        Call hitung()
+    End Sub
+    Private Sub byr5_TextChanged(sender As Object, e As EventArgs) Handles byr5.TextChanged
+        subtot = Val(harga) * Val(byr5.Text)
+        shrg5.Text = Format(subtot, "#,##")
+        n5 = subtot
+        Call hitung()
+    End Sub
+
+    Private Sub byr6_TextChanged(sender As Object, e As EventArgs) Handles byr6.TextChanged
+        subtot = Val(harga) * Val(byr6.Text)
+        shrg6.Text = Format(subtot, "#,##")
+        n6 = subtot
+        Call hitung()
+    End Sub
+    Sub hitung()
+        nsub = n1 + n2 + n3 + n4 + n5 + n6
+        txtotpem.Text = FormatCurrency(nsub)
+        If nsub >= 200000 Then
+            Dim nDis As Single
+            nDis = nsub * 0.15
+            txdiskon.Text = Format(nDis, "#,##")
+            tagihan = nsub - nDis
+        ElseIf nsub >= 100000 Then
+            Dim nDis As Single
+            nDis = nsub * 0.1
+            txdiskon.Text = Format(nDis, "#,##")
+            tagihan = nsub - nDis
+        Else Dim nDis As Single
+            nDis = nsub * 0
+            txdiskon.Text = Format(nDis, "#,##")
+            tagihan = nsub - nDis
+            'MessageBox.Show("TOTAL PEMBELIAN KURANG DARI 100 RIBU TIDAK MENDAPAT DISKON")
+        End If
+        txtagihan.Text = FormatCurrency(tagihan)
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        cb1.Checked = False
+        cb2.Checked = False
+        cb3.Checked = False
+        cb4.Checked = False
+        cb5.Checked = False
+        cb6.Checked = False
+    End Sub
+End Class
